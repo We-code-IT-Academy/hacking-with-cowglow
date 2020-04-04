8b1ac1168987c090a240e51092c60d13b22b9b6b
Philip Saa <cowglow@gmail.com>
2020-04-04 12:27:02 +0200

    Moved Binding to top of file and included a local storage strategy

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-function AddTodo(todoText) {
-    const element = document.createElement("LI")
-    const elementText = document.createTextNode(todoText)
-    element.appendChild(elementText)
-
-    outputListElement.appendChild(element)
-}
-
-// Handlers
-function formHandler (event) {
-    event.preventDefault();
-    AddTodo(inputFieldElement.value)
-}
+const todoCollection = [];
+const localStorageKey = "todos"
 
 // Binding
 const formElement = document.getElementById("todoInput")
 formElement.addEventListener("submit", formHandler)
 
 const inputFieldElement = document.getElementById("inputField")
-
 const outputListElement = document.getElementById("todoOutput")
+
+// Local Storage
+const data = JSON.parse(localStorage.getItem(localStorageKey))
+if (data && data.length > 0) {
+    // pre-load the stored data
+    data.forEach(item => {
+        AddTodo(item)
+    })
+}
+
+function AddTodo(todoText) {
+    const element = document.createElement("LI")
+    const elementText = document.createTextNode(todoText)
+    element.appendChild(elementText)
+
+    // Update the View
+    outputListElement.appendChild(element)
+
+    // Update the Model
+    todoCollection.push(todoText);
+    localStorage.setItem(localStorageKey, JSON.stringify(todoCollection))
+}
+
+// Handlers
+function formHandler (event) {
+    event.preventDefault();
+    AddTodo(inputFieldElement.value)
+    inputFieldElement.value = ""
+}
