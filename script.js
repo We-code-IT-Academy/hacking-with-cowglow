46db60de6c3402f58ebc657a8120d2df9607a7fb
dieuser <hubgit2020@gmail.com>
2020-04-21 10:05:29 +0200

    fix indention

--- script.js
+++ script.js
@@ -18,65 +18,64 @@ const outputListElement = document.getElementById("todoOutput")
 // Local Storage
 const data = JSON.parse(localStorage.getItem(localStorageKey))
 if (data && data.length > 0) {
-    // pre-load the stored data
-    data.forEach(item => {
-        AddTodo(item)
-    })
+	// pre-load the stored data
+	data.forEach(item => {
+		AddTodo(item)
+	})
 }
 
 function AddTodo(todoText) {
-        const element = document.createElement("LI");
+	const element = document.createElement("LI");
 	const elementBtn = document.createElement("button");
 	const elementText = document.createTextNode(todoText);
-	var elementId = nameId+loop;
+	var elementId = nameId + loop;
 	loop++;
 	elementBtn.id = elementId;
 	element.id = elementId;
 	elementBtn.type = "submit";
 	elementBtn.innerHTML = "x";
-	elementBtn.addEventListener("click",function(){ removeHandler(elementBtn.id); });
+	elementBtn.addEventListener("click", function () { removeHandler(elementBtn.id); });
 
 	element.appendChild(elementText);
 	element.appendChild(elementBtn);
 
-	
-    // Update the View
+
+	// Update the View
 	outputListElement.appendChild(element);
 
-    // Update the Model
-        todoCollection.push(todoText);
+	// Update the Model
+	todoCollection.push(todoText);
 	localStorage.setItem(localStorageKey, JSON.stringify(todoCollection));
-	
-}
-function RemTodo(todoText) {
-	
-	  item = document.getElementById(todoText);
-	  outputListElement.removeChild(item);
 
 }
-function RemAllTodo(){
-	while (outputListElement.hasChildNodes()) {  
-        outputListElement.removeChild(outputListElement.firstChild);
+function RemTodo(todoText) {
+
+	item = document.getElementById(todoText);
+	outputListElement.removeChild(item);
+
+}
+function RemAllTodo() {
+	while (outputListElement.hasChildNodes()) {
+		outputListElement.removeChild(outputListElement.firstChild);
 	}
 }
 
 
 // Handlers
-function addHandler (event) {
+function addHandler(event) {
 	event.preventDefault();
-	if (inputFieldElement.value.length==0) {alert("Empty Field");}
-	else
-	{
-        AddTodo(inputFieldElement.value);
-	inputFieldElement.value = "";
+	if (inputFieldElement.value.length == 0) { alert("Empty Field"); }
+	else {
+		AddTodo(inputFieldElement.value);
+		inputFieldElement.value = "";
 	}
 }
-function removeHandler (elementToRemoveId) {
+function removeHandler(elementToRemoveId) {
 	event.preventDefault();
 	RemTodo(elementToRemoveId);
 
 }
-function removeAllHandler (event) {
+function removeAllHandler(event) {
 	event.preventDefault();
 	RemAllTodo();
 	inputFieldElement.value = "";
